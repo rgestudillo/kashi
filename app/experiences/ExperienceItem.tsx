87058918548ac8ef058b0649e3a7b2b1ca4dbd0d
Kashi Estudillo <rgestudillo@up.edu.ph>
2024-05-19 01:33:55 +0800

    fixed text color

--- app/experiences/ExperienceItem.tsx
+++ app/experiences/ExperienceItem.tsx
@@ -22,7 +22,7 @@ export const ExperienceItem = ({
     <div className="mb-6 border-b pb-6 border-zinc-700">
       <div className="flex items-center justify-between mb-2">
         <Reveal>
-          <span className="font-bold text-xl">{title}</span>
+          <span className="font-bold text-xl text-black">{title}</span>
         </Reveal>
         <Reveal>
           <span className="text-black">{time}</span>
